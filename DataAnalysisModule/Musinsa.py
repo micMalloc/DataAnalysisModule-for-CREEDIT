40b508130eff7bf354efb3878577570d32ee7e00
Heesu Lee <hesue615@gmail.com>
2020-06-09 17:12:25 +0900

    Modify code - remove

--- DataAnalysisModule/Musinsa.py
+++ DataAnalysisModule/Musinsa.py
@@ -1,25 +1,20 @@
 from selenium import webdriver
 driver = webdriver.Chrome('C:/Users/yws72/Desktop/chromedriver.exe')
 
-from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-html = urlopen('https://store.musinsa.com/app/items/lists/001001')
-bsObject = BeautifulSoup(html, "html.parser")
-bs1 = bsObject.find('ul', {'id':'searchList'})
-
-best_page_urls = []
-for cover in bs1.find_all('div', {'class':'list_img'}):
-    link = cover.select('a')[0].get('href')
-    best_page_urls.append(link)
-
-print(best_page_urls)
-
-for url in best_page_urls:
-    driver.get("https://store.musinsa.com" + url)
+page_url = []
+for page in range(4,5):
+    driver.get('https://store.musinsa.com/app/items/lists/001001/?category=&d_cat_cd=001001&u_cat_cd=&brand=&sort=pop&sub_sort=&display_cnt=90&page=' + str(page))
     html = driver.page_source
-    soup = BeautifulSoup(html, 'html.parser')
-    pd_name = soup.find('div', {'class':'explan_product product_info_section'}).find_all('p', {'class':'product_article_contents'})[0].get_text()
-    pd_ss = soup.find('div', {'class':'explan_product product_info_section'}).find_all('p', {'class': 'product_article_contents'})[1].get_text()
-    pd_view = soup.find('div', {'class': 'explan_product product_info_section'}).find_all('p', {'class': 'product_article_contents'})[2].get_text()
-    print(pd_name, pd_ss, pd_view)
+    bsObject = BeautifulSoup(html, "html.parser")
+    bs1 = bsObject.find('ul', {'id':'searchList'})
+    bs2 = bs1.find_all('div', {'class':'list_img'})
+    for cover in bs2:
+        link = cover.select('a')[0].get('href')
+        page_url.append(link)
+
+target = open('test.csv', mode='w', encoding='ansi')
+header = 'brand,ss,view,qty,like,satisfaction,purchase,delivery,delivery_day,goods_price,list_price,point,touch,flex,trans,thick,season,color,ingredient,guarantee,totalview,view_age18,view_age24,' \
+         'view_age34,view_age44,view_age45,view_male,view_female,view_unknown,qa,rvcount,rvstyle,rvphoto,rvgoods\n'
+
