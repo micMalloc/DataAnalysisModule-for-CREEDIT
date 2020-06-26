f2298606564d7eb7697fa8be2e40fe238c6104a0
Heesu Lee <hesue615@gmail.com>
2020-06-26 18:02:34 +0900

    Add initial method for Launcher

---
+++ Batch/Launcher.py
@@ -0,0 +1,18 @@
+from Job.Job import Job
+
+class Launcher:
+    jobList
+
+    def __init__(self, PATH=None):
+        pass
+
+    def loadMetaData(self):
+        pass
+
+    def makeJobQueue(self):
+        pass
+    
+    def start(self):
+        pass
+
+    pass
