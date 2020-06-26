4fd5d95eb737c27b96654e7dff55649c0fcef312
Heesu Lee <hesue615@gmail.com>
2020-06-26 15:43:40 +0900

    Add parent directory to system path for using other modules

--- Batch/Analyzer/Analyzer.py
+++ Batch/Analyzer/Analyzer.py
@@ -1,9 +1,17 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+from Job.Job import Job
 from abc import *
-from Job import Job
+
 
 class DataAnalyzer(Job):
 
     _metaclass_=ABCMeta
+
+    def __init__(self):
+        pass
 
     def doJob(self):
         self.analyzeData()
--- Batch/Collector/Collector.py
+++ Batch/Collector/Collector.py
@@ -1,10 +1,17 @@
-from Job import Job
-from abc import *
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
+
+from abc import *
+from Job.Job import Job
 
 class DataCollector(Job):
     
     _metaclass_=ABCMeta
+
+    def __init__(self):
+        pass
 
     def doJob(self):
         self.collectData()
