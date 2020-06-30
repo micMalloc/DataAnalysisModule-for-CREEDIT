ac0d87db7b7ef4db13701dc2ae47d70a68f9b916
Heesu Lee <hesue615@gmail.com>
2020-06-30 16:49:04 +0900

    Implements Constructor for Launcher Class

--- Batch/Launcher.py
+++ Batch/Launcher.py
@@ -1,18 +1,28 @@
 from Job.Job import Job
+import json
 
 class Launcher:
-    jobList
 
     def __init__(self, PATH=None):
-        pass
+        if PATH is not None:
+            self.metaData = self.loadMetaData(PATH)
+            self.jobList = self.makeJobQueue()
+        else:
+            self.metaData = None
+        
+        print(self.metaData)
 
-    def loadMetaData(self):
-        pass
-
+    def loadMetaData(self, PATH):
+        with open(PATH) as jsonFile:
+            return json.load(jsonFile)
+            
     def makeJobQueue(self):
         pass
     
     def start(self):
         pass
 
+
+if __name__ == "__main__":
+    Launcher("/Users/heesu.lee/DataAnalysisModule-for-CREEDIT/Batch/meta.json")
     pass
