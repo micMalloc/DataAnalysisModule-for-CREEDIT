881142f205ed76c86766709524fa7632e616667e
Heesu Lee <hesue615@gmail.com>
2020-07-11 15:36:21 +0900

    Adjust Code

--- Batch/Job/Analyzer/Analyzer.py
+++ Batch/Job/Analyzer/Analyzer.py
@@ -2,7 +2,7 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-from Job import Job
+from . import Job.Job
 from abc import *
 
 
--- Batch/Job/Collector/Collector.py
+++ Batch/Job/Collector/Collector.py
@@ -2,9 +2,8 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-
 from abc import *
-from Job import Job
+from . import Job
 
 class DataCollector(Job):
     
