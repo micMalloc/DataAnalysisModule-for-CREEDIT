54edc82312be551885a3efcf0bcb5010af690f0d
kangbeonghyun <zxcvb8802@naver.com>
2020-07-27 21:46:41 +0900

    test action 07272146

---
+++ testaction.py
@@ -0,0 +1,12 @@
+import os
+
+print(os.environ['DB_ID'])
+print(os.environ['DB_IP_ADDRESS'])
+print(os.environ['DB_PASSWORD'])
+print(os.environ['GOOGLE_API_KEY'])
+
+print(type(os.environ['DB_ID']))
+print(type(os.environ['DB_IP_ADDRESS']))
+print(type(os.environ['DB_PASSWORD']))
+print(type(os.environ['GOOGLE_API_KEY']))
+
