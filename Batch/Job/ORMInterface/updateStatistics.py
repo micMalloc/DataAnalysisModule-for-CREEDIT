619b953cd645f34e80e3ec341261a0eac8d0376e
kangbeonghyun <zxcvb8802@naver.com>
2020-08-04 22:51:06 +0900

    update updatestatistics 08042250

--- Batch/Job/ORMInterface/updateStatistics.py
+++ Batch/Job/ORMInterface/updateStatistics.py
@@ -55,8 +55,8 @@ if __name__=="__main__":
 import os
 import pymysql
 from datetime import datetime, timedelta, date
-creedit = pymysql.connect(host='203.245.30.13', user='ehdgus93', password='ehdgus93!', db='db_creedit', charset='utf8')
-#creedit = pymysql.connect(host=os.environ['DB_IP_ADDRESS'], user=os.environ['DB_ID'], password=os.environ['DB_PASSWORD'], db='db_creedit', charset='utf8')
+
+creedit = pymysql.connect(host=os.environ['DB_IP_ADDRESS'], user=os.environ['DB_ID'], password=os.environ['DB_PASSWORD'], db='db_creedit', charset='utf8')
 # creedit = pymysql.connect(host='localhost', user='root', password='', db='creedit', charset='utf8')
 manager = creedit.cursor()
 
