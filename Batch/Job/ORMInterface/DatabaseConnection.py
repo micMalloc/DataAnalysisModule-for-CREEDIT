54887b7a10b13bf9339a6782e2b3c616760a60e1
kangbeonghyun <44596480+kangbeonghyun@users.noreply.github.com>
2020-08-07 00:10:02 +0900

    Update DatabaseConnection.py
    test

--- Batch/Job/ORMInterface/DatabaseConnection.py
+++ Batch/Job/ORMInterface/DatabaseConnection.py
@@ -6,7 +6,10 @@ from sqlalchemy.orm import sessionmaker
 
 
 engine = create_engine('mysql+mysqldb://' + os.environ['DB_ID']+":"+os.environ['DB_PASSWORD']+"@"+os.environ['DB_IP_ADDRESS']+'/db_creedit', convert_unicode=True, echo=True)
-
+print(os.environ['DB_ID'])
+temps=os.environ['DB_ID']
+print(temp)
+print("a"+os.environ['DB_ID']+"a")
 metadata=MetaData()
 metadata.reflect(engine,only=['stat','categorymap','channels','statistics'])
 #metadata.reflect(engine,only=['stat','categorymap','channels'])
