58962c355b14016a8fc160c21b757b4fdf3c1109
kangbeonghyun <44596480+kangbeonghyun@users.noreply.github.com>
2020-08-07 00:02:14 +0900

    Update updateStatistics.py
    test print dbid

--- DataAnalysisModule/updateStatistics.py
+++ DataAnalysisModule/updateStatistics.py
@@ -1,7 +1,7 @@
 import pymysql
 import os
 from datetime import datetime, timedelta, date
-)
+
 db_ip = str(os.environ['DB_IP_ADDRESS'])
 db_id = str(os.environ['DB_ID'])
 db_pw = str(os.environ['DB_PASSWORD'])
